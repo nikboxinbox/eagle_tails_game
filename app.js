5f2e8970b7c8990e799e95af664f0225cfa4ace2
nik nik <nikbox88@gmail.com>
2022-11-05 17:29:38 +0300

    + error input & clean

--- app.js
+++ app.js
@@ -16,18 +16,26 @@ const log = (logData) => {
 
 const getAnswerPromise = () => {
     return new Promise((resolve, reject) => {
-        input.question("Угадай 1 или 2 ? \n", (answer) => {
-            resolve(answer);
-        });
+        input.question(
+            // "Угадай 1 или 2 ? \n",
+            "",
+            (answer) => {
+                resolve(answer);
+            }
+        );
     });
 };
 
 const startGame = async () => {
     log("Новая партия");
+    console.log("Угадай 1 или 2");
     while (true) {
         const hiddenNum = Math.floor(Math.random() * 2) + 1;
         const userAnswer = await getAnswerPromise();
-        if (userAnswer == hiddenNum) {
+        if (userAnswer != 1 && userAnswer != 2) {
+            console.log("ответ должен быть 1 или 2");
+            log("Ошибка ввода");
+        } else if (userAnswer == hiddenNum) {
             console.log("Угадал!");
             log("Угадал");
         } else {
