1921cfcb41b4b93c4ee221f4d65ebac74e35d1d7
nik nik <nikbox88@gmail.com>
2022-11-05 15:57:04 +0300

    +game & log file

---
+++ app.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const readline = require("readline");
+const fs = require("fs");
+const input = readline.createInterface(process.stdin, process.stdout);
+const format = require("node.date-time");
+
+const fileName = process.argv[2];
+
+const logTime = () => {
+    return new Date().format("Y-M-d H:M:S") + " ";
+};
+
+const log = (logData) => {
+    console.log(fileName);
+    fs.appendFile(`${fileName}.log`, `${logTime()} ${logData} \n`, () => {});
+};
+
+const getAnswerPromise = () => {
+    return new Promise((resolve, reject) => {
+        input.question("Угадай 1 или 2 ? \n", (answer) => {
+            resolve(answer);
+        });
+    });
+};
+
+const startGame = async () => {
+    log("Новая партия");
+    while (true) {
+        const hiddenNum = Math.floor(Math.random() * 2) + 1;
+        const userAnswer = await getAnswerPromise();
+        if (userAnswer == hiddenNum) {
+            console.log("Угадал!");
+            log("Угадал");
+        } else {
+            console.log("Не угадал!");
+            log("Не угадал");
+        }
+    }
+};
+
+startGame();
